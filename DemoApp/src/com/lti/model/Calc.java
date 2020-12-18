fce0e9c5e7815c2bca8b580e2e38a775296a0124
Prajwal-vishwanath <76148911+Prajwal-vishwanath@users.noreply.github.com>
2020-12-18 13:05:26 +0530

    added division method

--- DemoApp/src/com/lti/model/Calc.java
+++ DemoApp/src/com/lti/model/Calc.java
@@ -13,5 +13,7 @@ public class Calc {
 	public void Mul(int num1, int num2) {
 		System.out.println("Multiplication="+(num1*num2));
 	}
-
+	public void div(int num1, int num2) {
+		System.out.println("Division="+(num1/num2));
+	}
 }
