1c037d1ec9e13fc159954600d289180d5a3dd9bd
Prajwal <Prajwal@LAPTOP-C5OH21RL>
2020-12-18 15:34:27 +0530

    deleted delete method

--- DemoApp/src/com/lti/model/Calc.java
+++ DemoApp/src/com/lti/model/Calc.java
@@ -13,8 +13,6 @@ public class Calc {
 	public void Mul(int num1, int num2) {
 		System.out.println("Multiplication="+(num1*num2));
 	}
-	public void div(int num1, int num2) {
-		System.out.println("Division="+(num1/num2));
-	}
+	
 	
 }
